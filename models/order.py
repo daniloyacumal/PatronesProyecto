a8bf3d993961d7267c74f72e2a7f5c9f8e2ceb78
daniloyacumal <daniloyacumalm@gmail.com>
2026-02-24 14:31:05 -0500

    Update order.py

--- models/order.py
+++ models/order.py
@@ -5,14 +5,28 @@ class Order:
         self.base_amount = base_amount
         self.observers = []
         self.status = "CREADO"
+        self.payment_strategy = None
 
+    # ===== Observer =====
     def attach(self, observer):
         self.observers.append(observer)
 
+    def detach(self, observer):
+        self.observers.remove(observer)
+
     def notify(self):
         for observer in self.observers:
-            observer.update(f"Orden {self.description} cambió a {self.status}")
+            observer.update(self)
 
     def change_status(self, status):
         self.status = status
         self.notify()
+
+    # ===== Strategy =====
+    def set_payment_strategy(self, strategy):
+        self.payment_strategy = strategy
+
+    def calculate_final_cost(self):
+        if not self.payment_strategy:
+            raise Exception("No se ha definido una estrategia de pago.")
+        return self.payment_strategy.calculate(self.base_amount)
