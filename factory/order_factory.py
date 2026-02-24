3386fdcf56560264756022b9d8d9baba7d35d724
daniloyacumal <daniloyacumalm@gmail.com>
2026-02-23 21:36:40 -0500

    Update order_factory.py

--- factory/order_factory.py
+++ factory/order_factory.py
@@ -1,7 +1,20 @@
-from models.order import Order
+from models.lab_order import LabOrder
+from models.medication_order import MedicationOrder
+from models.procedure_order import ProcedureOrder
 
 class OrderFactory:
 
     @staticmethod
-    def create_order(order_type, description, base_amount):
-        return Order(description, base_amount)
+    def create_order(order_type, description, base_amount, **kwargs):
+
+        if order_type == "lab":
+            return LabOrder(description, base_amount, kwargs.get("lab_type"))
+
+        elif order_type == "medication":
+            return MedicationOrder(description, base_amount, kwargs.get("dosage"))
+
+        elif order_type == "procedure":
+            return ProcedureOrder(description, base_amount, kwargs.get("complexity_level"))
+
+        else:
+            raise ValueError("Tipo de orden no válido")
