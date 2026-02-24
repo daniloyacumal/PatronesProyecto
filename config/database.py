3000c9009192587a15d07c5b5d301b05f05ec0b0
daniloyacumal <daniloyacumalm@gmail.com>
2026-02-24 14:29:40 -0500

    Update database.py

--- config/database.py
+++ config/database.py
@@ -3,9 +3,15 @@ class DatabaseConnection:
 
     def __new__(cls):
         if cls.__instance is None:
-            cls.__instance = super(DatabaseConnection, cls).__new__(cls)
-            cls.__instance.connection = "Database Connected"
+            cls.__instance = super().__new__(cls)
+            cls.__instance._initialized = False
         return cls.__instance
+
+    def __init__(self):
+        if self._initialized:
+            return
+        self.connection = "Database Connected"
+        self._initialized = True
 
     def get_connection(self):
         return self.connection
