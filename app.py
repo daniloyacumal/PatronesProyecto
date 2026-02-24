5e56e22e2b10dd84bf1aea7dff18f6e740c5d91b
daniloyacumal <daniloyacumalm@gmail.com>
2026-02-24 14:11:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+from config.database import DatabaseConnection
 from factory.order_factory import OrderFactory
 from strategies.eps_strategy import EPSStrategy
 from observers.patient_notifier import PatientNotifier
 from observers.pharmacy_notifier import PharmacyNotifier
 from observers.doctor_notifier import DoctorNotifier
 
-# Crear orden de laboratorio
+# Singleton
+db1 = DatabaseConnection()
+db2 = DatabaseConnection()
+
+print(db1.get_connection())
+print(db1 is db2)  # True
+
+# Factory: Crear orden de laboratorio
 order = OrderFactory.create_order(
     order_type="lab",
     description="Examen de sangre",
